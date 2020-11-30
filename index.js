32557918036e0160cd0b46e9339022d94763a02a
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-29 21:49:02 -0500

    added outline of categories to be added to the readme file

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//categories to be used in the README file
+// title and sections for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+
 // array of questions for user
 const questions = [
 
