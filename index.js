44d18539c50af407a154505439b84e73e9d32610
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-29 22:32:45 -0500

    added initial questions for the content to be added to the readme

--- index.js
+++ index.js
@@ -2,14 +2,39 @@
 const fs = require('fs');
 //added requirement to the inquirer module that will be used to ask questions and get the input that will be added to the readme file.
 const inquirer = require('inquirer');
-
+//added requirement to the "generateMarkdown" file that has the template literal for formatting the readme file content
+const generate = require('./utils/generateMarkdown.js');
 
 //categories to be used in the README file
 // title and sections for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your README file?',
+        name: 'title',
+    }
+    {
+        type: 'input',
+        message: 'What is a description of your program?',
+        name: 'description',
+    }
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'install',
+    }
+    {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'usage',
+    }
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contribute',
+    }
 
 ];
 
