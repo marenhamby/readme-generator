3b11da4f9d96d03dff13c6be9039ce327f469a53
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-29 22:45:20 -0500

    changed questions to be in inquirer prompt

--- index.js
+++ index.js
@@ -9,53 +9,56 @@ const generate = require('./utils/generateMarkdown.js');
 // title and sections for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // array of questions for user
-const questions = [
-    {
-        type: 'input',
-        message: 'What is the title of your README file?',
-        name: 'title',
-    }
-    {
-        type: 'input',
-        message: 'What is a description of your program?',
-        name: 'description',
-    }
-    {
-        type: 'input',
-        message: 'What are the installation instructions?',
-        name: 'install',
-    }
-    {
-        type: 'input',
-        message: 'What is the usage information?',
-        name: 'usage',
-    }
-    {
-        type: 'input',
-        message: 'What are the contribution guidelines?',
-        name: 'contribute',
-    }
-    {
-        type: 'list',
-        message: 'What is the license?',
-        name: 'license',
-        choices: ['choice1', 'choice2', 'choice3'],
-    }
-    {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'username',
-    }
-    {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email',
-    }
-];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your README file?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What is a description of your program?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions?',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'What is the usage information?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What are the contribution guidelines?',
+            name: 'contribute',
+        },
+        {
+            type: 'list',
+            message: 'What is the license?',
+            name: 'license',
+            choices: ['choice1', 'choice2', 'choice3'],
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+    ])
+   
+const questions =
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+    // function to write README file
+    function writeToFile(fileName, data) {
+    }
 
 // function to initialize program
 function init() {
