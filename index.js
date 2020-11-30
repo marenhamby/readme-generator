597d613dcc6bf071f2f034b71a488e04ab3a6e4e
marenh <72942304+marenhamby@users.noreply.github.com>
2020-11-29 21:55:53 -0500

    added requirements for inquirer and fs that will be used to create and add content to the readme file

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//added requirement for fs module that will be used to write the new readme file
+const fs = require('fs');
+//added requirement to the inquirer module that will be used to ask questions and get the input that will be added to the readme file.
+const inquirer = require('inquirer');
+
+
 //categories to be used in the README file
 // title and sections for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
